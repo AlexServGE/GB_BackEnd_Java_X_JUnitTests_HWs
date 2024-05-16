5606eb4c9e46b245ffacd03ae114d8fe1d37058c
AlexServGE <alexander.sergeevru@yandex.ru>
2024-05-16 06:45:43 +0300

    HW1 edited based on the teacher's comments

--- Homework1_Assert_AssertJ/CalculatorTest/src/main/java/org/example/Calculator.java
+++ Homework1_Assert_AssertJ/CalculatorTest/src/main/java/org/example/Calculator.java
@@ -2,8 +2,7 @@ package org.example;
 
 public class Calculator {
   public static double calculateDiscount(double purchaseAmount, double discountAmount) {
-    if (purchaseAmount < 1) throw new ArithmeticException();
-    if (discountAmount >= 1 || discountAmount <= 0) throw new ArithmeticException();
+    if (discountAmount > 1 || discountAmount < 0 || purchaseAmount < 1) throw new ArithmeticException();
     return purchaseAmount * discountAmount;
   }
 }
--- Homework1_Assert_AssertJ/CalculatorTest/src/main/java/org/example/Main.java
+++ Homework1_Assert_AssertJ/CalculatorTest/src/main/java/org/example/Main.java
@@ -7,8 +7,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class Main {
   public static void main(String[] args) {
     assertThat(Calculator.calculateDiscount(100.0, 0.5)).isEqualTo(50.0);
-    assertThatThrownBy(()->Calculator.calculateDiscount(100.0, 0)).isInstanceOf(ArithmeticException.class);
+    assertThatThrownBy(()->Calculator.calculateDiscount(100.0, -0.03)).isInstanceOf(ArithmeticException.class);
     assertThat(Calculator.calculateDiscount(100.0, 0.5)).isNotEqualTo(10.0);
-    assertThatThrownBy(()->Calculator.calculateDiscount(100.0, 0)).isInstanceOf(NullPointerException.class);
+    assertThatThrownBy(()->Calculator.calculateDiscount(100.0, 1.1)).isInstanceOf(NullPointerException.class);
   }
 }
